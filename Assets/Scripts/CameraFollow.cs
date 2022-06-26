81f9a01181504daa1c71a5d19752e6a3849f8579
FruitWave <broido@usc.edu>
2022-06-26 15:03:24 -0700

    Thia Level Design 1
    - added 3 tutorial challenges

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,19 +8,47 @@ public class CameraFollow : MonoBehaviour
     public GameObject circle;
     public GameObject rectangle;
     SwapGameObject stateComponent;
+
+    float x;
+    float y;
+    float z;
+    [SerializeField] bool isCircle;
     private void Start()
     {
-         stateComponent = player.GetComponent<SwapGameObject>();
+        stateComponent = player.GetComponent<SwapGameObject>();
+        x = 0f;
+        y = -0.25f;
+        z = -1f;
     }
     void Update()
     {
-        float x = 0f;
-        float y = -0.25f;
-        float z = -1f;
-        x = stateComponent.mState == SwapGameObject.State.Circle ? circle.transform.position.x : rectangle.transform.position.x;
-        //y = stateComponent.mState == SwapGameObject.State.Circle ? circle.transform.position.y : rectangle.transform.position.y;
-        
-
+        isCircle = stateComponent.mState == SwapGameObject.State.Circle ? true : false;
+        x = isCircle ? circle.transform.position.x : rectangle.transform.position.x;
+        setY(isCircle);
         gameObject.transform.position = new Vector3(x, y, z);
+    }
+    void setY(bool isCircle)
+    {
+        if (isCircle)
+        {
+            if (circle.transform.localPosition.y > 4.5 || circle.transform.localPosition.y < -10)
+            {
+                y = circle.transform.localPosition.y - 4.55f;
+            }
+            else
+            {
+                y = -0.25f;
+            }
+        } else
+        {
+            if (rectangle.transform.localPosition.y > 4.5 || rectangle.transform.localPosition.y < -10)
+            {
+                y = rectangle.transform.localPosition.y -4.55f;
+            }
+            else
+            {
+                y = -0.25f;
+            }
+        }
     }
 }
